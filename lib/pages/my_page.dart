695f29798edfe26e5af13035c4dd00b7bca7b77d
Gushiken <gusiken0604@gmail.com>
2023-09-24 11:41:42 +0900

    マイページ完成

--- lib/pages/my_page.dart
+++ lib/pages/my_page.dart
@@ -3,10 +3,119 @@ import 'package:flutter/material.dart';
 class MyPage extends StatelessWidget {
   const MyPage({Key? key}) : super(key: key);
 
+
+  final images = const [
+    'https://static.xx.fbcdn.net/rsrc.php/v3/y_/r/2wPYyq9Ejn4.png',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvoSuexQoraIJ3Xc8Qde7Y_A-7v7vfwWxveA&usqp=CAU',
+    'https://www.pakutaso.com/shared/img/thumb/kaigoIMGL8113.jpg',
+    'https://news.value-press.com/wp-content/uploads/interview_top_image_pakutaso.jpg',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('マイページ')),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+           crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Image.network(
+                  'https://static.xx.fbcdn.net/rsrc.php/v3/y_/r/2wPYyq9Ejn4.png',
+                  width: 60,
+                  height: 60,
+                ),
+                const Spacer(),
+                const Column(
+                  children: [
+                    Text(
+                      '7,041',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                    Text('投稿'),
+                  ],
+                ),
+                const SizedBox(
+                  width: 16,
+                ),
+                const Column(
+                  children: [
+                    Text(
+                      '4.6億',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                    Text('フォロワー'),
+                  ],
+                ),
+                const SizedBox(
+                  width: 16,
+                ),
+                const Column(
+                  children: [
+                    Text(
+                      '99',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                    Text('フォロー'),
+                  ],
+                ),
+              ],
+            ),
+            const Text(
+              'Instagram',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+              ),
+            ),
+            const Text(
+              '#YourToMake',
+              style: TextStyle(
+                color: Colors.blue,
+              ),
+            ),
+            const Text(
+              'help.Instagram.com',
+              style: TextStyle(
+                color: Colors.blue,
+              ),
+            ),
+            GridView.count(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              crossAxisCount: 3,
+              children: images.map((imageUrl) {
+                return InstagramPostItem(imageUrl: imageUrl);
+              }).toList(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class InstagramPostItem extends StatelessWidget {
+  const InstagramPostItem({Key? key, required this.imageUrl}) : super(key: key);
+
+  final String imageUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.network(
+      imageUrl,
+      fit: BoxFit.cover,
     );
   }
 }
