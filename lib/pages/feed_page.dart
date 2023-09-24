768e96831556eb0137386ac4a5a0b66f51ba0e9b
Gushiken <gusiken0604@gmail.com>
2023-09-24 14:16:26 +0900

    フィード追加

--- lib/pages/feed_page.dart
+++ lib/pages/feed_page.dart
@@ -7,6 +7,27 @@ class FeedPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('フィード')),
+      body: Column(
+        children: [
+          Image.network(
+              'https://static.xx.fbcdn.net/rsrc.php/v3/y_/r/2wPYyq9Ejn4.png',
+            fit: BoxFit.cover,
+
+          ),
+  Row(
+    children: [
+    Text('aa'),
+    Text('bb'),
+      Column(
+        children:[
+          Text('ccc'),
+          Text('dddd'),
+        ],
+      ),
+      ],
+  ),
+        ],
+      ),
     );
   }
 }
