26149971671b1ae52aec81b57dc8e70b52636161
Gushiken <gusiken0604@gmail.com>
2023-09-25 15:05:02 +0900

    画像を配列へ、スクロールできる。投稿はWidget

--- lib/pages/feed_page.dart
+++ lib/pages/feed_page.dart
@@ -3,31 +3,43 @@ import 'package:flutter/material.dart';
 class FeedPage extends StatelessWidget {
   const FeedPage({Key? key}) : super(key: key);
 
+
+
   @override
   Widget build(BuildContext context) {
+
+    final List<String> imageUrls = [
+      'https://static.xx.fbcdn.net/rsrc.php/v3/y_/r/2wPYyq9Ejn4.png',
+      'https://dol.ismcdn.jp/mwimgs/5/f/650/img_5f45e18281b17f8ce320b162b9df9aa1345777.jpg',
+      'https://dol.ismcdn.jp/mwimgs/e/7/600/img_e7e14cb9aa668fd143a7285707986c59209322.jpg'
+    ];
+
     return Scaffold(
       appBar: AppBar(title: const Text('フィード')),
-      body: Column(
-        children: [
-          Image.network(
-              'https://static.xx.fbcdn.net/rsrc.php/v3/y_/r/2wPYyq9Ejn4.png',
-            fit: BoxFit.cover,
+      body: SingleChildScrollView(
+      child:Column(
+        children: imageUrls.map((url) => PostWidget(imageUrl: url)).toList(),  // 配列を使って投稿を生成
+         // https://static.xx.fbcdn.net/rsrc.php/v3/y_/r/2wPYyq9Ejn4.png
+      ),
+      ),
+    );
+  }
+}
 
-          ),
-  Row(
-    children: [
-    Text('aa'),
-    Text('bb'),
-      Column(
-        children:[
-          Text('ccc'),
-          Text('dddd'),
-        ],
-      ),
+class PostWidget extends StatelessWidget {
+  final String imageUrl;
+
+  const PostWidget({Key? key, required this.imageUrl}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Image.network(
+          imageUrl,
+          fit: BoxFit.cover,
+        ),
       ],
-  ),
-        ],
-      ),
     );
   }
 }
